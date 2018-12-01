dfc1fa20c6692666decfbc147b75d5760a95fa87
Pedro Gomes <pedrogomes94@gmail.com>
2018-12-01 16:40:20 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var http = require('http');
+const http = require('http');
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-  res.write('Hello World!');
-  res.end();
-}).listen(5000);
+http
+  .createServer((req, res) => {
+    res.write('Hello World!');
+    res.end();
+  })
+  .listen(5000);
 
-console.log('Server listening on port '+ port);
-
+console.log(`Server listening on port ${port}`);
